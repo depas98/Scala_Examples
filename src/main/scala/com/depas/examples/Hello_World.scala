78492b964bdb18462ebdc188a3d41698c5b14512
depas98 <depas98@hotmail.com>
2017-11-16 21:28:34 -0700

    first scala program

---
+++ src/main/scala/com/depas/examples/Hello_World.scala
@@ -0,0 +1,15 @@
+package com.depas.examples
+
+object Hello_World {
+
+  def main (args: Array[String] ): Unit = {
+//    var n: Int = 2
+//    while( n <= 6){
+//      println(s"Hello ${n} bottles of beer")
+//      n += 1
+//    }
+
+    2 to 6 foreach { n => println(s"Hello ${n} bottles of beer")}
+  }
+}
+
